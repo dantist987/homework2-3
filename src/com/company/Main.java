90716bc48a2b424ef49e1a471a64ceff49c737e9
dantist987 <adik.isak2412@gmail.com>
2022-03-31 22:37:53 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> a = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+
+        System.out.println(a.toString());
+
+
+        ArrayList<String> b = new ArrayList<>();
+
+        System.out.println("------------------------");
+
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        System.out.println(b.toString());
+
+        System.out.println("--------------------");
+
+        ArrayList<String> c = new ArrayList<>();
+        Collections.reverse(b);
+        for (int i = 0; i < a.size(); i++) {
+            c.add(a.get(i));
+            c.add(b.get(i));
+        }
+        System.out.println(c);
+
+        System.out.println("---------------------");
+
+        StringComparator stringComparator = new StringComparator();
+        Collections.sort(c, stringComparator);
+        System.out.println(c);
+    }
+}
